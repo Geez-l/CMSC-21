b66105e7417b9114a01f2e08917248670dee2106
Gliezel Ann Pajarilla <93700998+Geez-l@users.noreply.github.com>
2022-03-03 14:57:40 +0700

    Add files via upload

---
+++ Lecture1/Reading-Assignment/Reading-Assignment/Page 38-ch3_ex1.c.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+   // creating a variable
+   int i, j;
+   float x, y;
+
+   // assigning values to the variables
+   i= 10;
+   j= 20;
+   x= 43.2892f;
+   y= 5527.0f;
+
+   // printing the value
+   printf("i=%d, j=%d, x= %f, y= %f\n", i,j,x,y);
+}
