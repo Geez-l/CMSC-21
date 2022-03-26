80e1c799de929cd5c854b325b33a2d75a2ee176b
Gliezel Ann Pajarilla <gtpajarilla@up.edu.ph>
2022-03-26 19:49:13 +0700

    update

--- Lecture 3/Assignments/as1.c
+++ Lecture 3/Assignments/as1.c
@@ -1,34 +1,22 @@
 #include <stdio.h>
-#include <stdbool.h>
-
-int main ()
 {
+  int age;
 
-  int age;                  //declared the variables
-  bool teenager = false;   // set value to false
-  printf("\nage: ");
-  scanf("%d", &age);
-  if (age >=13 && age <=19) // set the condition (assignemt = !0);
-  {
-   teenager = true;
-  }
-  printf("Teenager: %s", teenager ? "TRUE" : "FALSE"); //printing of results
-  return 0; 
+if (age >=13 && age <=19)
+  printf("teenager = TRUE");
+else:
+  printf ("teenager = FALSE");
+
+return 0;
+}
 #include <stdio.h>
-#include <stdbool.h>   /* system header file which 
-                     allows to use Boolean (True/ False)*/ 
-
-int main ()
 {
+  int age;
 
-  int age;                  //declared the variables
-  bool teenager = false;   // set value to false
-  printf("\nage: ");
-  scanf("%d", &age);
-  if (age >=13 && age <=19) // set the condition (assignemt = !0);
-  {
-   teenager = true;
-  }
-  printf("Teenager: %s", teenager ? "TRUE" : "FALSE"); //printing of results
-  return 0; 
+if (age >=13 && age <=19)
+  printf("teenager = TRUE");
+else:
+  printf ("teenager = FALSE");
+
+return 0;
 }
